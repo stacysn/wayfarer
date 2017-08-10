11a642f524c4b151214c7d56a51dae6ee8c28a37
Garrick Ngai <garrickdark@yahoo.com>
2017-08-10 14:17:05 -0700

    added icon to top and added initial seed data

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: 1,   city: 'San Francisco',country: 'United States'},
+  { id: 2,   city: 'London',country: 'Great Britain'},
+  { id: 3,   city: 'Seattle',country: 'United States'},
+  { id: 3,   city: 'Sydney',country: 'Australia'},
+]
+
+module.exports = data;
