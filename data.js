4e4e915433a349acea8e7c7aea531d6843695c1c
Garrick Ngai <garrickdark@yahoo.com>
2017-08-10 16:05:50 -0700

    created seed data and renamed components to proper naming

--- data.js
+++
@@ -1,8 +0,0 @@
-const data = [
-  { id: 1,   city: 'San Francisco',country: 'United States'},
-  { id: 2,   city: 'London',country: 'Great Britain'},
-  { id: 3,   city: 'Seattle',country: 'United States'},
-  { id: 4,   city: 'Sydney',country: 'Australia'},
-]
-
-module.exports = data;
