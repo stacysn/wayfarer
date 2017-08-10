58f6dcbd5706886d1ec5ae02e72eecf65b12dcfe
Garrick Ngai <garrickdark@yahoo.com>
2017-08-10 14:52:44 -0700

    moved files intro proper folder and created necessary component files

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = [
   { id: 1,   city: 'San Francisco',country: 'United States'},
   { id: 2,   city: 'London',country: 'Great Britain'},
   { id: 3,   city: 'Seattle',country: 'United States'},
-  { id: 3,   city: 'Sydney',country: 'Australia'},
+  { id: 4,   city: 'Sydney',country: 'Australia'},
 ]
 
 module.exports = data;
