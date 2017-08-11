b639c5867fd40c91001c5cfffd15e821ce37dc30
Christopher Fujino <christopherfujino@gmail.com>
2017-08-11 09:10:44 -0700

    cleanup whitespace

--- server.js
+++ server.js
@@ -36,132 +36,97 @@ router.get('/', function(req,res) {
   res.json({message: 'API Initialized!'});
 });
 
-//adding the /posts route to our /api router
-// router.route('/posts')
-//   //retrieve all posts from the db
-//   .get(function(req, res){
-//     //looks at our Post Schema
-//     Post.find(function(err, posts){
-//       if (err)
-//         res.send(err);
-//         res.json(posts);
-//     })
-//   })
-//   .post(function(req, res){
-//     var post = new Post();
-//     post.user = req.body.user;
-//     post.text = req.body.text;]
-//     post.date = req.body.date;
-//
-//     post.save(function(err){
-//       if (err)
-//         res.send(err);
-//         res.json({message: 'Post successfully added'});
-//     });
-//   });
-//
-// router.route('/posts/:id')
-//   //retrieve one specific post by id
-//   .get(function(req, res){
-//     Post.find(function(err, posts){
-//       if (err)
-//         res.send(err);
-//         res.json(posts)
-//     })
-//   })
-
-
 //adding the /cities route from our /api router
-  router.route('/cities')
-    //retrieve all posts from the db
-    .get(function(req, res){
-      //looks at our Post Schema
-      db.City.find(function(err, posts){
-        if (err)
-          res.send(err);
-          res.json(posts);
-      })
+router.route('/cities')
+  //retrieve all posts from the db
+  .get(function(req, res){  // GET all cities
+    //looks at our Post Schema
+    db.City.find(function(err, posts){
+      if (err)
+        res.send(err);
+        res.json(posts);
     })
-    .post(function(req, res){
-      var city = new db.City();
-      city.city = req.body.city; //change to name?
-      city.country = req.body.country;
-      city.img = req.body.img;
+  })
+  .post(function(req, res){
+    var city = new db.City();
+    city.city = req.body.city; //change to name?
+    city.country = req.body.country;
+    city.img = req.body.img;
 
-      city.save(function(err){
-        if (err)
-          res.send(err);
-          res.json({message: 'City post success!'});
+    city.save(function(err){
+      if (err)
+        res.send(err);
+        res.json({message: 'City post success!'});
+    });
+  });
+
+router.route('/cities/:id')
+  //retrive one specific id
+  .get(function(req, res){  // GET a city
+    db.City.findById(req.params.id, function(err, city){
+      if (err)
+        res.send(err);
+        res.json(city);
+    })
+  })
+  .delete(function(req, res){ // DELETE a city
+    db.City.findOneAndRemove({_id: req.params.cityId}, function(err){
+      if (err){
+        res.status(500);
+      }
+      res.json({message: 'City Deleted!'})
+    })
+  })
+
+router.route('/cities/:cityId/posts')
+  .get(function(req, res){  // GET all posts
+    db.City.findById(req.params.cityId, function(err, city) {
+      if (err) res.json(err);
+      res.json(city.posts);
+    });
+  })
+  .post(function(req, res){ // POST a new post
+    db.City.findById(req.params.cityId, function(err, city){
+      const doc = {
+        user: req.body.user,
+        text: req.body.text,
+        date: new Date()
+      };
+      var newPost = new db.Post(doc);
+      console.log("NEW POST", newPost);
+      city.posts.unshift(newPost);
+      city.save(function(err, savedCity){
+        if (err) res.json(err);
+        res.json(newPost);
       });
     });
-
-  router.route('/cities/:id')
-    //retrive one specific id
-    .get(function(req, res){
-      db.City.findById(req.params.id, function(err, city){
-        if (err)
-          res.send(err);
-          res.json(city);
-      })
-    })
-    .delete(function(req, res){
-      db.City.findOneAndRemove({_id: req.params.cityId}, function(err){
-        if (err){
-          res.status(500);
-        }
-        res.json({message: 'City Deleted!'})
-      })
-    })
-
-  router.route('/cities/:cityId/posts')
-    .get(function(req, res){
-      db.City.findById(req.params.cityId, function(err, city) {
-        if (err) res.json(err);
-        res.json(city.posts);
-      });
-    })
-    .post(function(req, res){
-      db.City.findById(req.params.cityId, function(err, city){
-        const doc = {
-          user: req.body.user,
-          text: req.body.text,
-          date: new Date()
-        };
-        var newPost = new db.Post(doc);
-        console.log("NEW POST", newPost);
-        city.posts.unshift(newPost);
-        city.save(function(err, savedCity){
-          if (err) res.json(err);
-          res.json(newPost);
-        });
-      });
-    })
+  })
 
 //route to delete specific post by id
-    router.route('/cities/:cityId/posts/:postId')
-      .delete(function(req, res){
-        db.City.findById(req.params.cityId, function(err, city){
-          let correctPost = city.posts.id(req.params.postId)
-            console.log("req.params.postId", req.params.postId);
-          if(correctPost){
-            correctPost.remove();
-            city.save(function(err, saved){
-              console.log("Removed ", correctPost);
-              res.status(200).send();
-            })
-          }
-          else{
-            return res.status(404).send("OH NO!!");
-          }
+router.route('/cities/:cityId/posts/:postId')
+  .delete(function(req, res){
+    db.City.findById(req.params.cityId, function(err, city){
+      let correctPost = city.posts.id(req.params.postId)
+        console.log("req.params.postId", req.params.postId);
+      if(correctPost){
+        correctPost.remove();
+        city.save(function(err, saved){
+          console.log("Removed ", correctPost);
+          res.status(200).send();
         })
-      })
-      .get(function (req, res) { // get one post
-        console.log("GET one post")
-        db.City.findById(req.params.cityId, function (err, city) {
-          if (err) return res.json(err);
-          res.json(city.posts.id(req.params.postId));
-        });
-      })
+      }
+      else{
+        return res.status(404).send("OH NO!!");
+      }
+    })
+  })
+  .get(function (req, res) { // get one post
+    console.log("GET one post")
+    db.City.findById(req.params.cityId, function (err, city) {
+      if (err) return res.json(err);
+      res.json(city.posts.id(req.params.postId));
+    });
+  })
 
 //start server
 app.listen(port, function() {
