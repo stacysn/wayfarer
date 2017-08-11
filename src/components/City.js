5a0004ace14c8644e28e4a61606bc383aa69a4f0
Christopher Fujino <christopherfujino@gmail.com>
2017-08-11 15:41:23 -0700

    added: posts to city show

--- src/components/City.js
+++ src/components/City.js
@@ -9,11 +9,37 @@ class City extends Component {
       description: '',
       posts: []
     };
-    console.log('in City.js, city ===', city);
+    const postComponents = city.posts.map(post => {
+      return (
+        <a className="list-group-item">
+          <h4 className="list-group-item-heading">{post.user}</h4>
+          <p className="list-group-item-text">{post.text}</p>
+        </a>
+      )
+    });
     return (
       <div className="col-md-8">
-        <h2>City Show</h2>
-        <h2>{city.city}</h2>
+        <div className="media">
+          <div className="media-body">
+            <h2>{city.city}</h2>
+          </div>
+          <div className="media-right">
+            <img className="media-object"
+              src={city.image}
+              alt={city.city}
+              style={{
+                height: '200px',
+                width: '300px'
+              }}>
+            </img>
+          </div>
+        </div>
+        <div>
+          <p>{city.description}</p>
+          <div className="list-group">
+            {postComponents}
+          </div>
+        </div>
       </div>
     )
   }
