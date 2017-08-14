a90be91400d5d6a35b1cd3453cb5dfd8abac79c8
Christopher Fujino <christopherfujino@gmail.com>
2017-08-13 18:53:39 -0700

    changed: `User.username` to `User.name`

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ let mongoose = require ("mongoose");
 let Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-  username: String,
+  name: String,
   password: String,
   hometown: String
 })
