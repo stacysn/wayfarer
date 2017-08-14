d3323fed8223742fa81ecb8c3fd5f7374ea836ff
Christopher Fujino <christopherfujino@gmail.com>
2017-08-13 18:45:04 -0700

    fixed: schema erroneously included id

--- models/User.js
+++ models/User.js
@@ -4,8 +4,7 @@ let Schema = mongoose.Schema;
 let UserSchema = new Schema({
   username: String,
   password: String,
-  hometown: String,
-  id: String
+  hometown: String
 })
 
 module.exports = mongoose.model('User', UserSchema);
