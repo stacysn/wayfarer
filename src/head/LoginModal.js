ec59f5de61d88fab9f91b7bdd544f1b2284b7dfb
stacysn <stacysuen5@gmail.com>
2017-10-10 11:29:33 -0700

    naming convention

--- src/head/LoginModal.js
+++ src/head/LoginModal.js
@@ -5,46 +5,50 @@ class LoginModal extends Component{
   constructor(props){
     super(props);
     this.state = {
-      name: '',
+      username: '',
       password: ''
     };
   }
   render(){
     return(
       <Modal show={this.props.isOpen} onHide={this.props.closeModal}>
+
         <Modal.Header closeButton>
           <Modal.Title id="ModalHeader">
             Log In
           </Modal.Title>
         </Modal.Header>
+
         <form onSubmit={e => {
           e.preventDefault();
           console.log('form submitted');
-          this.props.login(this.state.name, this.state.password);
-          this.setState({name: '', password: ''});
+          this.props.login(this.state.username, this.state.password);
+          this.setState({username: '', password: ''});
           this.props.closeModal();
         }}>
           <Modal.Body>
             <div className="form-group">
               <label htmlFor="recipient-name" className="control-label">Username
-                <input name="name" type="text"
+                <input username="name" type="text"
                   className="form-control"
-                  onChange={(e)=>this.setState({name:e.target.value})}
+                  onChange={(e)=>this.setState({username:e.target.value})}
                 />
               </label>
             </div>
             <div className="form-group">
               <label className="control-label">Password
-                <input name="password" type="password"
+                <input username="password" type="password"
                   className="form-control"
                   onChange={e=>this.setState({password:e.target.value})}
                 />
               </label>
             </div>
           </Modal.Body>
+
           <Modal.Footer>
             <button type="submit" className="btn btn-primary">Log In</button>
           </Modal.Footer>
+
         </form>
       </Modal>
     )
